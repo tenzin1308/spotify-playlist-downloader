6937b2395d68f04d68c3d03cb74648bb9741e10f
tenzin1308 <56772873+tenzin1308@users.noreply.github.com>
2021-07-20 13:40:09 -0400

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import { useEffect } from 'react';
+import SpotifyWebApi from "spotify-web-api-js";
 import './App.css';
+import { useDataLayerValue } from './DataLayer';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [{ token }, dispatch] = useDataLayerValue();
+
+  useEffect(() => {
+    // Set token
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    let _token = hash.access_token;
+
+    if (_token) {
+      spotify.setAccessToken(_token);
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      spotify.getPlaylist("2ILLL6Ep7x6ahuHoSHvptu").then((response) =>
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      );
+    }
+  }, [token, dispatch]);
   return (
     <div className="app">
-      <h1>Hello World</h1>
+      <Login />
+
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Login.css'
+import { loginUrl } from './spotify'
+
+function Login() {
+    return (
+        <div className="login">
+            <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png" alt=""/>
+        </div>
+    )
+}
+
+export default Login
