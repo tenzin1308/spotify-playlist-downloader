0348acbccc024807ef28c2ec2c315134013669ad
tenzin1308 <56772873+tenzin1308@users.noreply.github.com>
2021-07-22 17:26:50 -0400

    updated script file

--- python/downloader.py
+++ python/downloader.py
@@ -1,8 +1,8 @@
 # libaries
 from __future__ import unicode_literals
 import os
-import sys
 import ssl
+import json
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -12,41 +12,50 @@ from selenium.webdriver.support import expected_conditions as EC
 import youtube_dl
 
 
-song_name = sys.argv[1]
-song_artist = sys.argv[2]
+# path for chromedriver
+PATH = "/Users/tenzintashi/Downloads/chromedriver"
+
+# File path
+file_path = '/Users/tenzintashi/Colz/Coding Files/spotify-playlist-downloader/'
+file_name = 'dataset.txt'
 
 
-def retriveYTLink(song_name, song_artist):
-    # path for chromedriver
-    PATH = "/Users/tenzintashi/Downloads/chromedriver"
-    print(song_artist)
-    print(song_name)
+def download(file_path, file_name, PATH):
+    contains = ''
+    lst = []
+    with open(os.path.join(file_path, file_name), 'r') as file_reader:
+        contains = file_reader.read()
 
-    # use headless Chrome as the browser
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.binary_location = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-    driver = webdriver.Chrome(executable_path=PATH,
-                              options=chrome_options)
+    contains = json.loads(contains)
 
-    # Website to access
-    driver.get(
-        "https://www.youtube.com/results?search_query=+"+song_name+"+"+song_artist+"+official+music+video")
+    songs = list(contains.keys())
+    artists = list(contains.values())
 
-    hrefs = [video.get_attribute('href')
-             for video in driver.find_elements_by_id("video-title")]
+    for index in range(len(songs)):
+        song = songs[index]
+        artist = ' '.join(artists[index])
 
-    link = None
-    List = []
-    for href in hrefs:
-        if href is not None:
-            link = href
-            break
-    print(link)
-    return link
+        # use headless Chrome as the browser
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.binary_location = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+        driver = webdriver.Chrome(executable_path=PATH,
+                                  options=chrome_options)
 
+        # Website to access
+        driver.get(
+            "https://www.youtube.com/results?search_query=+"+song+"+"+artist+"+official+music+video")
 
-def download(song_name, song_artist):
+        hrefs = [video.get_attribute('href')
+                 for video in driver.find_elements_by_id("video-title")]
+
+        link = None
+        for href in hrefs:
+            if href is not None:
+                link = href
+                break
+        lst.append(link)
+
     ssl._create_default_https_context = ssl._create_unverified_context
     ydl_opts = {
         'format': 'bestaudio/best',
@@ -58,8 +67,8 @@ def download(song_name, song_artist):
     }
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([retriveYTLink(song_name, song_artist)])
+        ydl.download(lst)
 
 
-download(song_name, song_artist)
+download(file_path=file_path, file_name=file_name, PATH=PATH)
 exit()
